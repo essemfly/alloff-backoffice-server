88970c43a17ff68ee13079740f872bb4dc44ce6f
Seokmin Lee <essemfly@gmail.com>
2021-11-03 23:49:54 +0900

    Implement received item and inventory

---
+++ tagger/models/inventory.py
@@ -0,0 +1,25 @@
+from django.db import models
+from tagger.core.mongo.models.order import OrderType
+
+
+class InventoryStatus(models.TextChoices):
+    IN_STOCK = "IN_STOCK"
+    SHIPPED = "SHIPPED"
+
+
+class ProductType(models.TextChoices):
+    NORMAL_PRODUCT = "NORMAL"
+    TIMEDEAL_PRODUCT = "TIMEDEAL"
+
+
+class Inventory(models.Model):
+    code = models.CharField(max_length=30, db_index=True)
+    status = models.CharField(max_length=30)
+    size = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    product_id = models.CharField(max_length=50)
+    product_type = models.CharField(max_length=50, choices=ProductType.choices)
+
+
+# 출고 만들기
