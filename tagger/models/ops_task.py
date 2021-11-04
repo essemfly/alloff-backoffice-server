01b5e2c32d9402e585c7d839a26e2e26a9f90aaf
Steve Cheong <j.stephen.cheong@gmail.com>
2021-11-04 11:35:53 +0900

    Dashboard

---
+++ tagger/models/ops_task.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class OpsTaskType(models.TextChoices):
+    SOURCING = "SOURCING"
+    PAYMENT = "PAYMENT"
+    SOURCING = "SOURCING"
+    SOURCING = "SOURCING"
+    SOURCING = "SOURCING"
+    SOURCING = "SOURCING"
+
+
+
+class OrderTask(models.Model):
+    pass
