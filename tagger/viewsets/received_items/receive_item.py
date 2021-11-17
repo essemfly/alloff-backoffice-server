9c49a0db3db16e54a9cff37d59e3729db75cd833
Steve Cheong <j.stephen.cheong@gmail.com>
2021-11-17 17:27:20 +0900

    Fixed inventory not having images issue

--- tagger/viewsets/received_items/receive_item.py
+++ tagger/viewsets/received_items/receive_item.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 
+from bson import ObjectId
+
+from tagger.core.mongo.models.alloff_product import AlloffProduct
 from tagger.core.mongo.models.order import OrderType
+from tagger.core.mongo.models.product import Product
 from tagger.models import Inventory, ReceivedItem
 from tagger.models.inventory import InventoryStatus, ProductType
 
@@ -17,6 +21,10 @@ def make_inventory_with_received_item(ri: ReceivedItem) -> Inventory:
     if ri.needs_processing:
         code += "-NP"
 
+    pid = ObjectId(ri.product_id)
+    p = Product.objects(id=pid).first() if i.product_type == ProductType.NORMAL_PRODUCT \
+        else AlloffProduct.objects(id=pid).first()
+
     return Inventory.objects.create(
         code=code,
         status=InventoryStatus.PROCESSING_NEEDED if ri.needs_processing else InventoryStatus.IN_STOCK,
@@ -26,4 +34,5 @@ def make_inventory_with_received_item(ri: ReceivedItem) -> Inventory:
         product_name=ri.item_name,
         product_brand=ri.brand_keyname,
         product_type=ProductType.NORMAL_PRODUCT if ri.order_type == OrderType.NORMAL_ORDER else ProductType.TIMEDEAL_PRODUCT,
+        images=p.images
     )
