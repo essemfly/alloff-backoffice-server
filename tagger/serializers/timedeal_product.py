687cc22fde97172bc94b041b9ca5b3d901e57806
seokmin <essemfly@gmail.com>
2021-12-10 05:26:18 +0900

    Revert for brandid

--- tagger/serializers/timedeal_product.py
+++ tagger/serializers/timedeal_product.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from os import remove
 from rest_framework_mongoengine.serializers import (
     DynamicDocumentSerializer,
 )
@@ -6,6 +7,7 @@ from rest_framework import serializers
 
 from tagger.core.mongo.models.brand import Brand
 from tagger.core.mongo.models.alloff_product import AlloffProduct, AlloffProductTemplate
+from tagger.models import inventory
 
 
 class TimedealProductTemplateSerializer(DynamicDocumentSerializer):
@@ -21,15 +23,16 @@ class TimedealProductSerializer(DynamicDocumentSerializer):
 
 
 class TimedealProductAddSerializer(DynamicDocumentSerializer):
+    brandid = serializers.CharField()
+
     class Meta:
         model = AlloffProduct
         fields = [
             "canceldescription",
             "deliverydescription",
             "sizedescription",
-            "brand",
-            "instruction.description",
-            "instruction.title",
+            "brandid",
+            "instruction",
             "faults",
             "description",
             "originalprice",
@@ -44,9 +47,49 @@ class TimedealProductAddSerializer(DynamicDocumentSerializer):
         ]
 
     def create(self, validated_data):
-        prod = AlloffProduct(**validated_data)
-        prod.brand._id = validated_data.get("brandid")
-        prod.save()
+        brand: Brand = Brand.objects.get(id=validated_data.get("brandid"))
+        brand_object = {
+            "_id": str(brand.id),
+            "keyname": brand.keyname,
+            "korname": brand.korname,
+            "engname": brand.engname,
+            "logoimgurl": brand.logoimgurl,
+            "onpopular": brand.onpopular,
+            "description": brand.description,
+            "isopen": brand.isopen,
+            "ishide": brand.ishide,
+            "modulename": brand.modulename,
+            "maxdiscountrate": brand.maxdiscountrate,
+            "numnewproducts": brand.numnewproducts,
+            "sizeguide": brand.sizeguide,
+            "category": brand.category,
+            "created": brand.created,
+            "inmaintenance": False,
+        }
+
+        prod: AlloffProduct = AlloffProduct.objects.create(
+            brand=brand_object,
+            canceldescription=validated_data.get("canceldescription"),
+            deliverydescription=validated_data.get("deliverydescription"),
+            description=validated_data.get("description"),
+            discountedprice=validated_data.get("discountedprice"),
+            discountrate=validated_data.get("discountrate"),
+            faults=validated_data.get("faults"),
+            images=validated_data.get("images"),
+            instruction=validated_data.get("instruction"),
+            name=validated_data.get("name"),
+            originalprice=validated_data.get("originalprice"),
+            producttype=validated_data.get("producttype"),
+            sizedescription=validated_data.get("sizedescription"),
+            productgroupid=validated_data.get("productgroupid"),
+            inventory=validated_data.get("inventory"),
+            soldout=validated_data.get("soldout"),
+            created=datetime.now(),
+            updated=datetime.now(),
+            removed=False,
+        )
+        # TO BE FIXED: Serializer와 viewset에서의 관계에 대한 공부 필요.
+        prod.brandid = validated_data.get("brandid")
         return prod
 
     def update(self, instance, validated_data):
