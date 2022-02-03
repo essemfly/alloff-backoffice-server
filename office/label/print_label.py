e4f4162426542dec7a0a152ceeee7f22e942d7b3
Stephen Cheong <j.stephen.cheong@gmail.com>
2022-02-03 13:54:02 +0900

    Restored label printing

--- office/label/print_label.py
+++ office/label/print_label.py
@@ -1,11 +1,13 @@
 import requests
 
-from alloff_backoffice_server.settings import LABEL_SERVER_URL
+from alloff_backoffice_server.settings import LABEL_SERVER_URL, SERVICE_ENV_IS_DEV
 
 
 def print_label(label_xml: str):
-    print(label_xml)
-    # requests.post(LABEL_SERVER_URL,
-    #               data=label_xml.encode('utf-8'),
-    #               headers={'Content-type': 'text/plain; charset=utf-8'}
-    #               )
+    if SERVICE_ENV_IS_DEV:
+        print(label_xml)
+    else:
+        requests.post(LABEL_SERVER_URL,
+                    data=label_xml.encode('utf-8'),
+                    headers={'Content-type': 'text/plain; charset=utf-8'}
+                    )
