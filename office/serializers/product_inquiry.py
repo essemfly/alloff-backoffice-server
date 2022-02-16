60e507996a1d970ac098f1af450c88bf1617d473
Stephen Cheong <j.stephen.cheong@gmail.com>
2022-02-17 02:49:57 +0900

    Added product to inquiries

--- office/serializers/product_inquiry.py
+++ office/serializers/product_inquiry.py
@@ -1,8 +1,12 @@
 from django_grpc_framework import proto_serializers
+from drf_spectacular.utils import extend_schema_field
 from office.serializers.pagination import PaginationSerializer
 from office.serializers.user_recorded_model import WithUserSerializer
+from product.serializers.product import ProductSerializer
+from product.services.product import ProductService
 from protos.order.product_inquiry import product_inquiry_pb2
 from protos.order.product_inquiry_reply import product_inquiry_reply_pb2
+from protos.product.product_pb2 import GetProductRequest
 from rest_framework import fields
 
 
@@ -15,12 +19,13 @@ class ProductInquiryReplySerializer(WithUserSerializer):
     class Meta:
         proto_class = product_inquiry_reply_pb2.ProductInquiryReply
 
+
 class ProductInquirySerializer(proto_serializers.ProtoSerializer):
     id = fields.IntegerField()
     product_id = fields.CharField(allow_null=False)
     brand_keyname = fields.CharField(allow_null=False)
     company_keyname = fields.CharField(allow_null=False)
-    
+
     user_id = fields.CharField(allow_null=False)
     user_name = fields.CharField(allow_null=False)
     body = fields.CharField(allow_null=False)
@@ -31,8 +36,19 @@ class ProductInquirySerializer(proto_serializers.ProtoSerializer):
     deleted_at = fields.DateTimeField(allow_null=True)
 
     replies = ProductInquiryReplySerializer(many=True)
+
+    product = fields.SerializerMethodField()
+
+    @extend_schema_field(ProductSerializer)
+    def get_product(self, obj):
+        req = GetProductRequest(alloff_product_id=obj.product_id)
+        pd = ProductService.get(req)
+        serializer = ProductSerializer(pd)
+        return serializer.data
+
     class Meta:
         proto_class = product_inquiry_pb2.ProductInquiry
+
 
 class PaginatedProductInquirySerializer(PaginationSerializer):
     results = ProductInquirySerializer(many=True)
