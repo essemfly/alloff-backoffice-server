8d74344c7492ee8041fd816d803c654949746088
Stephen Cheong <j.stephen.cheong@gmail.com>
2022-02-17 14:51:15 +0900

    Tidied up after conflict merge

--- product/viewsets/product.py
+++ product/viewsets/product.py
@@ -6,23 +6,6 @@ from protos.product.product_pb2 import (GetProductRequest, ListProductsRequest,
 from rest_framework import mixins, status, viewsets
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
-from rest_framework import status
-
-from alloff_backoffice_server.settings import PAGE_SIZE
-from product.serializers.product import (
-    CreateProductRequestSerializer,
-    EditProductRequestSerializer,
-    ListProductResultSerializer,
-    ListProductSerializer,
-    ProductSerializer,
-)
-from product.services.product import ProductService
-from protos.product.product_pb2 import (
-    GetProductRequest,
-    ListProductsRequest,
-    ProductQuery,
-)
-
 
 from product.helpers.get_module_name import get_module_name
 from product.serializers.product import (CreateProductRequestApiSerializer,
@@ -77,7 +60,7 @@ class ProductViewSet(
         res = ProductService.list(req)
         serializer = ListProductResultSerializer(res)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
+
     @with_company_api
     def retrieve(self, request, pk, *args, **kwargs):
         module_name = get_module_name(request)
@@ -85,9 +68,12 @@ class ProductViewSet(
         req = GetProductRequest(alloff_product_id=pk)
         pd = ProductService.get(req)
         serializer = ProductSerializer(pd)
-        # if module_name is not None and serializer.data.get("module_name") != module_name:
+        if (
+            module_name is not None
+            and serializer.data.get("module_name") != module_name
+        ):
             # Raise a 404.
-            # raise NotFound("Product not found.")
+            raise NotFound("Product not found.")
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     @extend_schema(
