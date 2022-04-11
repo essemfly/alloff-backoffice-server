e5284aa27cb1071674ade2013113bf851b9a0678
Stephen Cheong <j.stephen.cheong@gmail.com>
2022-04-12 00:12:06 +0900

    Removed unnecessary shell script files

--- build_and_push_ecs.sh
+++
@@ -1,3 +0,0 @@
-docker buildx build --platform=linux/amd64 -t backoffice-server .
-docker tag backoffice-server:latest 193142563486.dkr.ecr.ap-northeast-2.amazonaws.com/backoffice-server:latest
-docker push 193142563486.dkr.ecr.ap-northeast-2.amazonaws.com/backoffice-server:latest
